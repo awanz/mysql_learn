c21a27384014e201f49415e776631487915b8639
Awan <me.awan.es@gmail.com>
2023-01-19 20:59:45 +0700

    update insert

--- 0018_insert.sql
+++ 0018_insert.sql
@@ -1,2 +1,17 @@
+--  single insert
 INSERT INTO Persons (ID, FirstName, LastName, Age)
 VALUES (1, 'Cardinal', 'Erichsen', 22);
+
+-- multi insert
+INSERT INTO Persons (ID, FirstName, LastName, Age)
+VALUES 
+(2, 'Cardinal', 'Erichsen', 22),
+(3, 'Cardinal', 'Erichsen', 22),
+(4, 'Cardinal', 'Erichsen', 22),
+(5, 'Cardinal', 'Erichsen', 22),
+(6, 'Cardinal', 'Erichsen', 22),
+(7, 'Cardinal', 'Erichsen', 22),
+(8, 'Cardinal', 'Erichsen', 22),
+(9, 'Cardinal', 'Erichsen', 22),
+(10, 'Cardinal', 'Erichsen', 22)
+;
